a956d8daa659c7a99f60266ac2777fc4e4cba11d
Kaustubh Garud <kaustubh.garud@hexagon.com>
2023-06-09 17:31:00 +0530

    final2

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}
     >
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" href="#">
           {props.title}
         </Link>
         <button
@@ -25,15 +25,15 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
-              </Link>
+              </a>
             </li>
-            <li className="nav-item" >
-              <Link className="nav-link" to="/about">
+            {/* <li className="nav-item" >
+              <a className="nav-link" href="/about">
                 {props.aboutText}
-              </Link>
-            </li>
+              </a>
+            </li> */}
           </ul>
           {/* <form className="d-flex" role="search">
             <input
@@ -48,31 +48,7 @@ export default function Navbar(props) {
           </form> */}
         </div>
       </div>
-      <div className="form-check1" onClick={props.switchMode} style={{color: props.mode==='dark'?'white':'black'}}>
-        <input
-          className="form-check-input"
-          type="radio"
-          name="flexRadioDefault"
-          id="flexRadioDefault1"
-        />
 
-        <label className="form-check-label" htmlFor="flexRadioDefault1" >
-          Orange mode radio
-        </label>
-
-      </div>
-      <div className="form-check2">
-        <input
-          className="form-check-input"
-          type="radio"
-          name="flexRadioDefault"
-          id="flexRadioDefault2"
-          checked
-        />
-        <label className="form-check-label" htmlFor="flexRadioDefault2">
-          Default checked radio
-        </label>
-      </div>
       <div
         className={`form-check form-switch text-${
           props.mode === "light" ? "dark" : "light"
